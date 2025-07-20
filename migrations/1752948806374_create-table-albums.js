7acd4d504bda82222d3e9ed420e3e682b591f45c
muham <muhambriana.31@gmail.com>
2025-07-20 11:23:07 +0700

    feat: create migration for albums table with necessary fields

---
+++ migrations/1752948806374_create-table-albums.js
@@ -0,0 +1,35 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('albums', {
+    rec_id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    album_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'integer',
+      notNull: true,
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('albums');
+};
