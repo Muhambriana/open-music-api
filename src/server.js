1fa1561498079321fcb9db27a0c8a2900cbade47
muham <muhambriana.31@gmail.com>
2025-07-20 14:42:47 +0700

    feat: initialize server with Hapi framework and album management plugin

---
+++ src/server.js
@@ -0,0 +1,50 @@
+import dotenv from 'dotenv';
+import Hapi from '@hapi/hapi';
+import AlbumsService from './services/postgres/albums/AlbumsService.js';
+import AlbumsValidator from './validator/albums';
+import ClientError from './exceptions/ClientError';
+import albumsPlugin from './api/albums';
+
+dotenv.config();
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+
+  const server = Hapi.server({
+    port: process.env.POST,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugins: albumsPlugin,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  server.ext('onPreResponse', (request, h) => {
+    const { response } = request;
+
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: 'fail',
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
