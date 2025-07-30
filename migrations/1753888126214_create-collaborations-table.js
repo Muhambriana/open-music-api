f60920caad90e1e205f30e4b879f56e245291025
muham <muhambriana.31@gmail.com>
2025-07-30 22:15:51 +0700

    feat: add collaborations table with unique constraint on playlist_id and user_id

---
+++ migrations/1753888126214_create-collaborations-table.js
@@ -0,0 +1,43 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('collaborations', {
+    rec_id: {
+      type: 'serial',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'integer',
+      notNull: true,
+      references: 'playlists(rec_id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+    user_id: {
+      type: 'integer',
+      notNull: true,
+      references: 'users(rec_id)',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  });
+
+  pgm.addConstraint('collaborations', 'unique_playlist_id_and_user_id', 'UNIQUE(playlist_id, user_id)');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('collaborations');
+};
